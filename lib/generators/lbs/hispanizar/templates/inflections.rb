ab6815bdff06ad385046f032d0606910708d6e7b
Azarel Doroteo Pacheco <azarel.doroteo@logicalbricks.com>
2012-04-17 17:17:52 -0500

    Inflections update

--- lib/generators/lbs/hispanizar/templates/inflections.rb
+++ lib/generators/lbs/hispanizar/templates/inflections.rb
@@ -3,19 +3,26 @@
 ActiveSupport::Inflector.inflections do |inflect|
   inflect.plural(/([rndl])([A-Z]|_|$)/, '\1es\2')
   inflect.plural(/([aeiou])([A-Z]|_|$)/, '\1s\2')
-  inflect.plural(/([aeiou])([A-Z]|_)([a-z]+)([rndl])([A-Z]|_|$)/, '\1s\2\3\4es\5')
-  inflect.plural(/([rndl])([A-Z]|_)([a-z]+)([aeiou])([A-Z]|_|$)/, '\1es\2\3\4s\5')
+  inflect.plural(/([aeiou])([A-Z]|_)([a-z]+)([rndl])([A-Z]|_|$)/, 
+                 '\1s\2\3\4es\5')
+  inflect.plural(/([rndl])([A-Z]|_)([a-z]+)([aeiou])([A-Z]|_|$)/, 
+                 '\1es\2\3\4s\5')
   inflect.plural(/(z)$/i, 'ces')
 
+  final_plural_rndl = /([aeiou][rndl])es([A-Z]|_|$)/
+  final_plural_vocal = /((?<![aeiou][nrdl])[aeiou])s([A-Z]|_|$)/
+  palabra_compuesta_1 = /#{final_plural_rndl}([a-z]+)#{final_plural_vocal}/
+  palabra_compuesta_2 = /#{final_plural_vocal}([a-z]+)#{final_plural_rndl}/
+
   inflect.singular(/(ia)$/i, '\1')
-  inflect.singular(/([aeiou])s([A-Z]|_|$)/, '\1\2')
-  inflect.singular(/([rndl])es([A-Z]|_|$)/, '\1\2')
-  inflect.singular(/([aeiou])s([A-Z]|_)([a-z]+)([rndl])es([A-Z]|_|$)/, '\1\2\3\4\5')
-  inflect.singular(/([rndl])es([A-Z]|_)([a-z]+)([aeiou])s([A-Z]|_|$)/, '\1\2\3\4\5')
+  inflect.singular(final_plural_rndl, '\1\2')
+  inflect.singular(final_plural_vocal, '\1\2')
+  inflect.singular(palabra_compuesta_1, '\1\2\3\4\5')
+  inflect.singular(palabra_compuesta_2, '\1\2\3\4\5')
   inflect.singular(/ces$/, 'z')
   
-  inflect.singular 'pais', 'pais'
   inflect.irregular 'pais', 'paises'
+  inflect.singular /(pais)([A-Z]|_|$)/, '\1\2'
 
   inflect.uncountable %w( campus lunes martes miercoles jueves viernes )
 end
