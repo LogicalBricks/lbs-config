eb5fb5929ece493587ca9d8338ef6f0da2fbfc40
Azarel Doroteo Pacheco <azarel.doroteo@logicalbricks.com>
2012-04-21 13:09:22 -0500

    Se agrega formtastic y simple_form

--- lib/generators/lbs/install/install_generator.rb
+++ lib/generators/lbs/install/install_generator.rb
@@ -30,8 +30,15 @@ module Lbs
       template("edit.html.#{engine_extension}", "lib/templates/#{engine_extension}/scaffold/edit.html.#{engine_extension}")
       template("new.html.#{engine_extension}", "lib/templates/#{engine_extension}/scaffold/new.html.#{engine_extension}")
       template("show.html.#{engine_extension}", "lib/templates/#{engine_extension}/scaffold/show.html.#{engine_extension}")
-      template("_form.html.#{engine_extension}", "lib/templates/#{engine_extension}/scaffold/_form.html.#{engine_extension}")
-      copy_file("_error_messages.html.#{engine_extension}", "app/views/application/_error_messages.html.#{engine_extension}")
+
+      if form == 'default'       
+        template("_form.html.#{engine_extension}", "lib/templates/#{engine_extension}/scaffold/_form.html.#{engine_extension}")
+        copy_file("_error_messages.html.#{engine_extension}", "app/views/application/_error_messages.html.#{engine_extension}")
+      elsif form == 'formtastic' 
+        template("_form_f.html.#{engine_extension}", "lib/templates/#{engine_extension}/scaffold/_form.html.#{engine_extension}")
+      elsif form == 'simple_form'
+        template("_form_s.html.#{engine_extension}", "lib/templates/#{engine_extension}/scaffold/_form.html.#{engine_extension}")
+      end
     end
 
     def copy_controller          
