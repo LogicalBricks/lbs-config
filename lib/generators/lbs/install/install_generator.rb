eb1ac8ea1a455e1a3ea3f9c90f66691705f23ed2
Azarel Doroteo Pacheco <azarel.doroteo@logicalbricks.com>
2012-05-29 16:34:06 -0500

    Small bux fixed about the destroy link for haml in index

--- lib/generators/lbs/install/install_generator.rb
+++ lib/generators/lbs/install/install_generator.rb
@@ -93,7 +93,7 @@ module Lbs
       v = '<%= singular_table_name %>'
       v = '@' + v if variable == :instance
       complete_syntax :destroy, v do
-        "= link_to t('.destroy', default: t('helpers.links.destroy')), #{v}, <%= key_value :confirm, \"t'helpers.links.confirm')\" %>, <%= key_value :method, ':delete' %>"
+        "= link_to t('.destroy', default: t('helpers.links.destroy')), #{v}, <%= key_value :confirm, \"t('helpers.links.confirm')\" %>, <%= key_value :method, ':delete' %>"
       end
     end
 
